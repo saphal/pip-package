ca66e3d0c5c59d0a6d56f92bacb8a2ee8efade53
Saphal Adhikari <franticarsenal@gmail.com>
2020-03-01 12:19:01 +0545

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='inheritance_py_sa',
+      version='0.1.0',
+      author='Saphal Adhikari',
+      author_email='franticarsenal@gmail.com',
+      description='Basic inheritance example',
+      packages=['inheritance_py_sa'],
+      zip_safe=False)
